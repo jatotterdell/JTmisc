c659a01b1814eefc951458799bd1aa76cec0fd98
James Totterdell <jatotterdell@gmail.com>
2023-02-27 10:34:56 +1100

    Revisions

--- R/distributions.R
+++ R/distributions.R
@@ -1,3 +1,5 @@
+# Bernoulli distribution ----
+
 #' @title Bernoulli distribution
 #' @description Calculate PMF for Bernoulli distribution
 #' @param x Value
@@ -19,6 +21,30 @@ rbern <- function(n, prob = 0.5) {
     stats::rbinom(n, size = 1, prob = prob)
 }
 
+
+# Categorical distribution ----
+
+#' @title Categorical distribution
+#' @description
+#' Random generation for categorical distribution.
+#' @param n number of observations.
+#' @param prob vector or matrix of probabilities where nrow(prob) = n.
+#' @return Random deviate
+#' @export
+rcategorical <- function(n, prob = c(0.5, 0.5)) {
+    if ("matrix" %in% class(prob)) {
+        stopifnot("nrow(prob) != n" = n == nrow(prob))
+        k <- ncol(prob)
+        y <- sapply(1:n, \(x) sample(seq_len(k), size = 1, prob = prob[x, ]))
+    } else {
+        k <- length(prob)
+        y <- sample(seq_len(k), size = n, prob = prob, replace = TRUE)
+    }
+    return(y)
+}
+
+
+# Cumulative logistic distribution ----
 
 #' @title expit(x)
 #' @description
@@ -78,11 +104,9 @@ cumulative_expit <- function(x, reverse = FALSE) {
 }
 
 
-#' cumulative_logit(x)
-#'
+#' @title Cumulative logistic function
 #' @description
 #' Transform from probabilities to cumulative logit cut-points.
-#'
 #' @param x The vector of probabilities
 #' @return cumulative_logit(x)
 #' @export
@@ -91,41 +115,36 @@ cumulative_logit <- function(x) {
 }
 
 
-#' @title Ordered logistic density
-#' @description
-#' @param x Value
-#' @param alpha Cut-points
-#' @param eta Shift in alpha, default = 0
-#' @param log Return log density, deafult FALSE
-#' @return Ordered logistic density function
-#' @export
-dordlogit <- function(x, alpha, eta = 0, log = FALSE) {
-    K <- length(alpha) + 1
-    stopifnot("x must be between 1 and K" = all(x > 0 & x < K + 1))
-    alpha <- c(-Inf, as.numeric(alpha), Inf)
-    p <- inv_logit(alpha[x + 1] - eta) - inv_logit(alpha[x] - eta)
-    if (log == TRUE) p <- log(p)
-    return(p)
-}
+# #' @title Ordered logistic density
+# #' @description
+# #' @param x Value
+# #' @param alpha Cut-points
+# #' @param eta Shift in alpha, default = 0
+# #' @param log Return log density, deafult FALSE
+# #' @return Ordered logistic density function
+# #' @export
+# dordlogit <- function(x, alpha, eta = 0, log = FALSE) {
+#     K <- length(alpha) + 1
+#     stopifnot("x must be between 1 and K" = all(x > 0 & x < K + 1))
+#     alpha <- c(-Inf, as.numeric(alpha), Inf)
+#     p <- inv_logit(alpha[x + 1] - eta) - inv_logit(alpha[x] - eta)
+#     if (log == TRUE) p <- log(p)
+#     return(p)
+# }
 
 
-rordlogit <- function(n, phi = 0, a) {
-    a <- c(as.numeric(a), Inf)
-    k <- 1:length(a)
-    if (length(phi) == 1) {
-        p <- dordlogit(k, a = a, phi = phi, log = FALSE)
-        y <- sample(k, size = n, replace = TRUE, prob = p)
-    } else {
-        # vectorized input
-        y <- rep(NA, n)
-        if (n > length(phi)) {
-            # need to repeat some phi values
-            phi <- rep(phi, ceiling(n / length(phi)))
-        }
-        for (i in 1:n) {
-            p <- dordlogit(k, a = a, phi = phi[i], log = FALSE)
-            y[i] <- sample(k, size = 1, replace = TRUE, prob = p)
-        }
-    }
-    y
-}
+# rordlogit <- function(n, alpha, eta = 0) {
+#     K <- length(alpha) + 1
+#     if (length(eta) == 1) {
+#         p <- dordlogit(seq_len(K), alpha, eta, log = FALSE)
+#         y <- rcategorical(n, p)
+#     } else {
+#         y <- rep(NA, n)
+#         stopifnot("length(eta) not equal to n" = n == length(eta))
+#         for (i in 1:n) {
+#             p <- dordlogit(seq_len(K), alpha, eta, log = FALSE)
+#             y[i] <- rcategorical(n, p)
+#         }
+#     }
+#     y
+# }
